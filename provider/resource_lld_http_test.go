4a07ad09900c25c8f690f13d5ac2e5034e1131e1
tpretz-bot-tars <bot-tars@pressnell.uk>
2026-02-20 15:09:34 +0000

    🦀 tests: randomize lld http acc test names

--- provider/resource_lld_http_test.go
+++ provider/resource_lld_http_test.go
@@ -1,12 +1,17 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccResourceLLDHttp(t *testing.T) {
+	id := resource.UniqueId()
+	groupName := "test-group-" + id
+	tmplHost := "test-template-" + id
+
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -14,13 +19,13 @@ func TestAccResourceLLDHttp(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: `
+				Config: fmt.Sprintf(`
 resource "zabbix_hostgroup" "testgrp" {
-  name = "test-group"
+  name = %q
 }
 resource "zabbix_template" "testtmpl" {
   groups = [zabbix_hostgroup.testgrp.id]
-  host   = "test-template"
+  host   = %q
 }
 
 resource "zabbix_lld_http" "testrule" {
@@ -37,7 +42,7 @@ resource "zabbix_lld_http" "testrule" {
   verify_peer    = false
   follow_redirects = true
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_lld_http.testrule", "key", "lld.http.discovery"),
 					resource.TestCheckResourceAttr("zabbix_lld_http.testrule", "name", "LLD HTTP Rule"),
@@ -47,13 +52,13 @@ resource "zabbix_lld_http" "testrule" {
 				),
 			},
 			{
-				Config: `
+				Config: fmt.Sprintf(`
 resource "zabbix_hostgroup" "testgrp" {
-  name = "test-group"
+  name = %q
 }
 resource "zabbix_template" "testtmpl" {
   groups = [zabbix_hostgroup.testgrp.id]
-  host   = "test-template"
+  host   = %q
 }
 
 resource "zabbix_lld_http" "testrule" {
@@ -70,7 +75,7 @@ resource "zabbix_lld_http" "testrule" {
   verify_peer    = true
   follow_redirects = false
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_lld_http.testrule", "key", "lld.http.discovery2"),
 					resource.TestCheckResourceAttr("zabbix_lld_http.testrule", "name", "LLD HTTP Rule A"),
