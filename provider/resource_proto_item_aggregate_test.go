4de452993ccdb2af719dd5cf99a3b6a40b3b7a4b
tpretz-bot-tars <bot-tars@pressnell.uk>
2026-02-20 16:32:46 +0000

    🦀 tests: randomize proto aggregate names

--- provider/resource_proto_item_aggregate_test.go
+++ provider/resource_proto_item_aggregate_test.go
@@ -1,50 +1,75 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccResourceProtoItemAggregate(t *testing.T) {
+	id := resource.UniqueId()
+	groupName := "test-group-" + id
+	tmplHost := "test-template-" + id
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "zabbix_hostgroup" "testgrp" { name = "test-group" }
-resource "zabbix_template" "testtmpl" { groups = [zabbix_hostgroup.testgrp.id] host = "test-template" }
-resource "zabbix_lld_simple" "rule" { hostid = zabbix_template.testtmpl.id key = "lld.simple.discovery" name = "LLD Simple Rule" }
+				Config: fmt.Sprintf(`
+resource "zabbix_hostgroup" "testgrp" {
+  name = %q
+}
+resource "zabbix_template" "testtmpl" {
+  groups = [zabbix_hostgroup.testgrp.id]
+  host   = %q
+}
+resource "zabbix_lld_simple" "rule" {
+  hostid = zabbix_template.testtmpl.id
+  key    = "lld.simple.discovery"
+  name   = "LLD Simple Rule"
+}
 
 resource "zabbix_proto_item_aggregate" "testitem" {
   ruleid = zabbix_lld_simple.rule.id
   hostid = zabbix_template.testtmpl.id
-  key = "aggregate.key"
-  name = "Proto Aggregate Item"
+  key    = "aggregate.key"
+  name   = "Proto Aggregate Item"
   valuetype = "unsigned"
   delay = "1m"
+  type = "0"
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_proto_item_aggregate.testitem", "delay", "1m"),
 				),
 			},
 			{
-				Config: `
-resource "zabbix_hostgroup" "testgrp" { name = "test-group" }
-resource "zabbix_template" "testtmpl" { groups = [zabbix_hostgroup.testgrp.id] host = "test-template" }
-resource "zabbix_lld_simple" "rule" { hostid = zabbix_template.testtmpl.id key = "lld.simple.discovery" name = "LLD Simple Rule" }
+				Config: fmt.Sprintf(`
+resource "zabbix_hostgroup" "testgrp" {
+  name = %q
+}
+resource "zabbix_template" "testtmpl" {
+  groups = [zabbix_hostgroup.testgrp.id]
+  host   = %q
+}
+resource "zabbix_lld_simple" "rule" {
+  hostid = zabbix_template.testtmpl.id
+  key    = "lld.simple.discovery"
+  name   = "LLD Simple Rule"
+}
 
 resource "zabbix_proto_item_aggregate" "testitem" {
   ruleid = zabbix_lld_simple.rule.id
   hostid = zabbix_template.testtmpl.id
-  key = "aggregate.key2"
-  name = "Proto Aggregate Item A"
+  key    = "aggregate.key2"
+  name   = "Proto Aggregate Item A"
   valuetype = "unsigned"
   delay = "30s"
+  type = "0"
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_proto_item_aggregate.testitem", "key", "aggregate.key2"),
 					resource.TestCheckResourceAttr("zabbix_proto_item_aggregate.testitem", "delay", "30s"),
