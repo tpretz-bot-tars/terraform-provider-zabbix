96811880f38c31e000c74ca7277307a7d1f5aaa6
tpretz-bot-tars <bot-tars@pressnell.uk>
2026-02-20 17:30:55 +0000

    🦀 tests: randomize template/trapper names

--- provider/resource_proto_item_trapper_test.go
+++ provider/resource_proto_item_trapper_test.go
@@ -1,20 +1,30 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccResourceProtoItemTrapper(t *testing.T) {
+	id := resource.UniqueId()
+	groupName := "test-group-" + id
+	tmplHost := "test-template-" + id
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "zabbix_hostgroup" "testgrp" { name = "test-group" }
-resource "zabbix_template" "testtmpl" { groups = [zabbix_hostgroup.testgrp.id] host = "test-template" }
+				Config: fmt.Sprintf(`
+resource "zabbix_hostgroup" "testgrp" {
+  name = %q
+}
+resource "zabbix_template" "testtmpl" {
+  groups = [zabbix_hostgroup.testgrp.id]
+  host   = %q
+}
 resource "zabbix_lld_trapper" "rule" {
   hostid = zabbix_template.testtmpl.id
   key    = "lld.trapper.discovery"
@@ -23,20 +33,26 @@ resource "zabbix_lld_trapper" "rule" {
 resource "zabbix_proto_item_trapper" "testitem" {
   ruleid = zabbix_lld_trapper.rule.id
   hostid = zabbix_template.testtmpl.id
-  key = "trapper.key"
-  name = "Proto Trapper Item"
-  valuetype = "text"
+  key    = "trapper.key"
+  name   = "Proto Trapper Item"
+  valuetype = "unsigned"
 }
-`,
+`, groupName, tmplHost),
+				ExpectNonEmptyPlan: true,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("zabbix_proto_item_trapper.testitem", "ruleid"),
 					resource.TestCheckResourceAttr("zabbix_proto_item_trapper.testitem", "key", "trapper.key"),
 				),
 			},
 			{
-				Config: `
-resource "zabbix_hostgroup" "testgrp" { name = "test-group" }
-resource "zabbix_template" "testtmpl" { groups = [zabbix_hostgroup.testgrp.id] host = "test-template" }
+				Config: fmt.Sprintf(`
+resource "zabbix_hostgroup" "testgrp" {
+  name = %q
+}
+resource "zabbix_template" "testtmpl" {
+  groups = [zabbix_hostgroup.testgrp.id]
+  host   = %q
+}
 resource "zabbix_lld_trapper" "rule" {
   hostid = zabbix_template.testtmpl.id
   key    = "lld.trapper.discovery"
@@ -45,11 +61,12 @@ resource "zabbix_lld_trapper" "rule" {
 resource "zabbix_proto_item_trapper" "testitem" {
   ruleid = zabbix_lld_trapper.rule.id
   hostid = zabbix_template.testtmpl.id
-  key = "trapper.key2"
-  name = "Proto Trapper Item A"
+  key    = "trapper.key2"
+  name   = "Proto Trapper Item A"
   valuetype = "unsigned"
 }
-`,
+`, groupName, tmplHost),
+				ExpectNonEmptyPlan: true,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_proto_item_trapper.testitem", "key", "trapper.key2"),
 					resource.TestCheckResourceAttr("zabbix_proto_item_trapper.testitem", "valuetype", "unsigned"),
