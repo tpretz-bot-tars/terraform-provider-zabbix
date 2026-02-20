6503d182604d2b3605b919a4b51e2ca40a462d09
tpretz-bot-tars <bot-tars@pressnell.uk>
2026-02-20 15:19:28 +0000

    🦀 tests: randomize lld internal acc test names

--- provider/resource_lld_internal_test.go
+++ provider/resource_lld_internal_test.go
@@ -1,12 +1,17 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccResourceLLDInternal(t *testing.T) {
+	id := resource.UniqueId()
+	groupName := "test-group-" + id
+	tmplHost := "test-template-" + id
+
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
@@ -14,13 +19,13 @@ func TestAccResourceLLDInternal(t *testing.T) {
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: `
+				Config: fmt.Sprintf(`
 resource "zabbix_hostgroup" "testgrp" {
-  name = "test-group"
+  name = %q
 }
 resource "zabbix_template" "testtmpl" {
   groups = [zabbix_hostgroup.testgrp.id]
-  host   = "test-template"
+  host   = %q
 }
 
 resource "zabbix_lld_internal" "testrule" {
@@ -28,20 +33,20 @@ resource "zabbix_lld_internal" "testrule" {
   key    = "lld.internal.discovery"
   name   = "LLD Internal Rule"
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_lld_internal.testrule", "key", "lld.internal.discovery"),
 					resource.TestCheckResourceAttr("zabbix_lld_internal.testrule", "name", "LLD Internal Rule"),
 				),
 			},
 			{
-				Config: `
+				Config: fmt.Sprintf(`
 resource "zabbix_hostgroup" "testgrp" {
-  name = "test-group"
+  name = %q
 }
 resource "zabbix_template" "testtmpl" {
   groups = [zabbix_hostgroup.testgrp.id]
-  host   = "test-template"
+  host   = %q
 }
 
 resource "zabbix_lld_internal" "testrule" {
@@ -49,7 +54,7 @@ resource "zabbix_lld_internal" "testrule" {
   key    = "lld.internal.discovery2"
   name   = "LLD Internal Rule A"
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_lld_internal.testrule", "key", "lld.internal.discovery2"),
 					resource.TestCheckResourceAttr("zabbix_lld_internal.testrule", "name", "LLD Internal Rule A"),
