ff1c865bbd393a211b2962bc748d693fd7aa4b37
tpretz-bot-tars <bot-tars@pressnell.uk>
2026-02-20 16:57:46 +0000

    🦀 tests: randomize proto simple test names

--- provider/resource_proto_item_simple_test.go
+++ provider/resource_proto_item_simple_test.go
@@ -1,20 +1,30 @@
 package provider
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccResourceProtoItemSimple(t *testing.T) {
+	id := resource.UniqueId()
+	groupName := "test-group-" + id
+	tmplHost := "test-template-" + id
+
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: `
-resource "zabbix_hostgroup" "testgrp" { name = "test-group" }
-resource "zabbix_template" "testtmpl" { groups = [zabbix_hostgroup.testgrp.id] host = "test-template" }
+				Config: fmt.Sprintf(`
+resource "zabbix_hostgroup" "testgrp" {
+  name = %q
+}
+resource "zabbix_template" "testtmpl" {
+  groups = [zabbix_hostgroup.testgrp.id]
+  host   = %q
+}
 
 resource "zabbix_lld_simple" "rule" {
   hostid = zabbix_template.testtmpl.id
@@ -25,20 +35,25 @@ resource "zabbix_lld_simple" "rule" {
 resource "zabbix_proto_item_simple" "testitem" {
   ruleid = zabbix_lld_simple.rule.id
   hostid = zabbix_template.testtmpl.id
-  key = "script[\"abc\"]"
-  name = "Proto Simple Item"
+  key    = "script[\"abc\"]"
+  name   = "Proto Simple Item"
   valuetype = "text"
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttrSet("zabbix_proto_item_simple.testitem", "ruleid"),
 					resource.TestCheckResourceAttr("zabbix_proto_item_simple.testitem", "name", "Proto Simple Item"),
 				),
 			},
 			{
-				Config: `
-resource "zabbix_hostgroup" "testgrp" { name = "test-group" }
-resource "zabbix_template" "testtmpl" { groups = [zabbix_hostgroup.testgrp.id] host = "test-template" }
+				Config: fmt.Sprintf(`
+resource "zabbix_hostgroup" "testgrp" {
+  name = %q
+}
+resource "zabbix_template" "testtmpl" {
+  groups = [zabbix_hostgroup.testgrp.id]
+  host   = %q
+}
 
 resource "zabbix_lld_simple" "rule" {
   hostid = zabbix_template.testtmpl.id
@@ -49,11 +64,11 @@ resource "zabbix_lld_simple" "rule" {
 resource "zabbix_proto_item_simple" "testitem" {
   ruleid = zabbix_lld_simple.rule.id
   hostid = zabbix_template.testtmpl.id
-  key = "scriptb[\"abc\"]"
-  name = "Proto Simple Item A"
+  key    = "scriptb[\"abc\"]"
+  name   = "Proto Simple Item A"
   valuetype = "unsigned"
 }
-`,
+`, groupName, tmplHost),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("zabbix_proto_item_simple.testitem", "key", "scriptb[\"abc\"]"),
 					resource.TestCheckResourceAttr("zabbix_proto_item_simple.testitem", "name", "Proto Simple Item A"),
